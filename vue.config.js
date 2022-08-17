cb2cd039e0c93d02b57f61142441b38c64a9ec1d
chaikd <gunhongchen@163.com>
2022-08-17 17:34:19 +0800

    fix webpack productionSourceMap

--- vue.config.js
+++ vue.config.js
@@ -19,6 +19,7 @@ module.exports = {
         disableHostCheck: true
     },
     runtimeCompiler: true,
+    productionSourceMap: false,
     css: {
         loaderOptions: {
             postcss: {
